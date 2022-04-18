243072e8c7d9b8c0066e122f984875908b8a463a
josh40110 <josh40110@gmail.com>
2022-04-18 10:44:39 +0800

    alter Listcompversus Gen

--- file/ListCompversusGenExp.py
+++ file/ListCompversusGenExp.py
@@ -1,7 +1,10 @@
 colors = ['black','white']
 sizes = ['L','M','S']
-tshirts = [(color,size) for color in colors for size in sizes]
-tshirts
+tshirts_listComprehensive = [(color,size) for color in colors for size in sizes] 
+tshirts_listComprehensive #in list comperhensive
+for tshirt in ('%s %s'%(c,s)for c in colors for s in sizes):
+    print(tshirt) #generator expression
+
 
 symbols = '^%$#&#%$#&#'
 tuple(ord(symbol)for symbol in symbols)
@@ -9,3 +12,4 @@ listcomp =[ord(symbol)for symbol in symbols]
 
 import array
 array.array('I',(ord(symbol)for symbol in symbols))
+#The first argument of the array constructor defines the storage type used for the numbers in the array
